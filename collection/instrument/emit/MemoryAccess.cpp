0cacaca313725519e4f8c293d177b321088ea107
Jakub Beránek <berykubik@gmail.com>
2017-10-27 23:30:58 +0200

    FIX: fix size calculation of loads

--- collection/instrument/emit/MemoryAccess.cpp
+++ collection/instrument/emit/MemoryAccess.cpp
@@ -1,6 +1,5 @@
 #include "MemoryAccess.h"
 
-#include <iostream>
 #include <llvm/IR/Module.h>
 #include <IR/ConstantsContext.h>
 
@@ -14,9 +13,13 @@ using namespace llvm;
 
 void MemoryAccess::handleStore(StoreInst* store, int32_t debugIndex)
 {
+    auto size = store->getModule()->getDataLayout().getTypeSizeInBits(
+            store->getValueOperand()->getType()
+    ) / 8;
+
     auto emitter = this->context.createEmitter(store);
     emitter.store(emitter.getBuilder().CreatePointerCast(store->getPointerOperand(), this->context.getTypes().voidPtr()),
-                  this->context.getValues().int64(store->getValueOperand()->getType()->getPrimitiveSizeInBits() / 8),
+                  this->context.getValues().int64(size),
                   this->getAddressSpace(store->getPointerAddressSpace()),
                   this->context.getValues().int64(this->context.getTypeMapper().mapType(store->getValueOperand()->getType())),
                   this->context.getValues().int32(debugIndex)
@@ -24,9 +27,13 @@ void MemoryAccess::handleStore(StoreInst* store, int32_t debugIndex)
 }
 void MemoryAccess::handleLoad(LoadInst* load, int32_t debugIndex)
 {
+    auto size = load->getModule()->getDataLayout().getTypeSizeInBits(
+            load->getPointerOperand()->getType()->getPointerElementType()
+    ) / 8;
+
     auto emitter = this->context.createEmitter(load);
     emitter.load(emitter.getBuilder().CreatePointerCast(load->getPointerOperand(), this->context.getTypes().voidPtr()),
-                 this->context.getValues().int64(load->getPointerOperand()->getType()->getPrimitiveSizeInBits() / 8),
+                 this->context.getValues().int64(size),
                  this->getAddressSpace(load->getPointerAddressSpace()),
                  this->context.getValues().int64(this->context.getTypeMapper().mapType(load->getType())),
                  this->context.getValues().int32(debugIndex)
@@ -36,8 +43,11 @@ Value* MemoryAccess::getAddressSpace(uint32_t addressSpace)
 {
     switch (static_cast<LLVMAddressSpace>(addressSpace))
     {
-        case LLVMAddressSpace::Shared: return this->context.getValues().int32(static_cast<uint32_t>(cupr::AddressSpace::Shared));
-        case LLVMAddressSpace::Constant: return this->context.getValues().int32(static_cast<uint32_t>(cupr::AddressSpace::Constant));
-        default: return this->context.getValues().int32(static_cast<uint32_t>(cupr::AddressSpace::Global));
+        case LLVMAddressSpace::Shared:
+            return this->context.getValues().int32(static_cast<uint32_t>(cupr::AddressSpace::Shared));
+        case LLVMAddressSpace::Constant:
+            return this->context.getValues().int32(static_cast<uint32_t>(cupr::AddressSpace::Constant));
+        default:
+            return this->context.getValues().int32(static_cast<uint32_t>(cupr::AddressSpace::Global));
     }
 }
--- collection/tests/test_access.py
+++ collection/tests/test_access.py
@@ -47,3 +47,31 @@ def test_access_type(profile, format):
 
     assert accesses[0]["kind"] == AccessType.Write
     assert accesses[1]["kind"] == AccessType.Read
+
+
+@param_all_formats
+def test_access_complex0(profile, format):
+    data = profile("""
+    #include <cstdio>
+    __global__ void kernel(float* a, float* b, float* c) {
+        int tid = threadIdx.x;
+        a[tid] = b[tid] + c[tid];
+    }
+    int main() {
+        float* dptr;
+        cudaMalloc(&dptr, sizeof(float));
+        printf("%p\\n", dptr);
+        kernel<<<1, 1>>>(dptr, dptr, dptr);
+        cudaFree(dptr);
+        return 0;
+    }
+    """, format=format, with_metadata=True)
+    accesses = data["mappings"][kernel_file("kernel", format=format)]["accesses"]
+
+    assert accesses[0]["kind"] == AccessType.Read
+    assert accesses[1]["kind"] == AccessType.Read
+    assert accesses[2]["kind"] == AccessType.Write
+
+    assert accesses[0]["size"] == 4
+    assert accesses[1]["size"] == 4
+    assert accesses[2]["size"] == 4
