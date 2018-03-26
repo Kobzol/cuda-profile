935b03c66fcc0c5ec17f53ef374e2715396fb181
Jakub Beránek <berykubik@gmail.com>
2018-03-26 17:19:37 +0200

    ENH: persist only selected items from store

--- visualization/src/lib/state/store.ts
+++ visualization/src/lib/state/store.ts
@@ -1,13 +1,13 @@
 import createHistory from 'history/createBrowserHistory';
 import {routerMiddleware, routerReducer} from 'react-router-redux';
-import {applyMiddleware, compose, createStore} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import {createLogger} from 'redux-logger';
 import {createEpicMiddleware} from 'redux-observable';
 import thunk from 'redux-thunk';
 import {rootEpic} from './epics';
 import {reducers} from './reducers';
-import {persistStore, persistCombineReducers} from 'redux-persist';
-import storage from 'redux-persist/es/storage';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import url from 'url';
 
 export const history = createHistory({
@@ -18,17 +18,26 @@ const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compo
 const router = routerMiddleware(history);
 const epic = createEpicMiddleware(rootEpic);
 const logger = createLogger();
-const persistConfig = {
-    key: 'root',
+
+const tracePersist = {
+    key: 'trace',
+    storage
+};
+const profilePersist = {
+    key: 'project',
     storage,
-    whitelist: ['trace', 'profile']
+    whitelist: ['profile']
 };
 
+const rootReducer = combineReducers({
+    ...reducers,
+    trace: persistReducer(tracePersist, reducers.trace),
+    profile: persistReducer(profilePersist, reducers.profile),
+    router: routerReducer
+});
+
 export const store = createStore(
-    persistCombineReducers(persistConfig, {
-        ...reducers,
-        router: routerReducer
-    }),
+    rootReducer,
     composeEnhancers(applyMiddleware(router, epic, thunk, logger))
 );
 export const persistor = persistStore(store);
